7b65f6986a2857eaa24d7c55f1a73eed4653507d
Alvaro Santa Cruz <alvaro.santacruz@pucp.pe>
2015-06-02 06:10:18 -0500

    Final

---
+++ Avatar.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstdio>
+#include "Avatar.h"
+//#include <string>
+//using namespace std;
+
+Avatar::Avatar(void){
+    nombre = "Guanira";
+    posX = 0;
+    posY = 0;
+    maxVida = 100;
+}
+Avatar::Avatar(string name,int x,int y,int health){
+    nombre = name;
+    posX = x;
+    posY = y;
+    maxVida = health;
+}
